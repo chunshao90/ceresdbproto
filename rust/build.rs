544940ab31702d090adad7945f9f2229678b3529
chunshao.rcs <chunshao.rcs@antgroup.com>
2023-02-13 14:33:33 +0800

    wip

--- rust/build.rs
+++ rust/build.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure().out_dir("./src").compile(
         &[
             "../protos/cluster.proto",
-            "../protos/common.proto",
+            "../protos/xx/common.proto",
             "../protos/meta_event.proto",
             "../protos/meta_service.proto",
             "../protos/meta_storage.proto",
